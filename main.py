93de464a7e3619786dee9e33c52672ebc90d8bb0
Hinnes <hinneslung@gmail.com>
2018-04-26 01:44:35 +0800

    submitted

--- main.py
+++ main.py
@@ -1,41 +1,7 @@
 import numpy as np
 import pandas as pd
 
+edge_df = pd.read_csv('./edges.csv')
 
-def load_dated_data(path, date_format='%d-%m-%Y %H:%M:%S'):
-    date_parser = lambda x: pd.datetime.strptime(x, date_format)
-    data = pd.read_csv(path, header=0, index_col=0, parse_dates=['Time'], date_parser=date_parser)
-    return data
-
-
-def load_data(path):
-    return pd.read_csv(path, header=0, index_col=0)
-
-
-f = {
-    'WarName': ['first'],
-    'WarType': ['first'],
-    # 'Ccode': ['list'],
-    # 'Player': ['list'],
-
-    'StartMonth': ['min'],
-    'StartDay': ['min'],
-    'StartYear': ['min'],
-    'EndMonth': ['max'],
-    'EndDay': ['max'],
-    'EndYear': ['max'],
-
-    # 'WhereFought': ['list'],
-    'Death': ['sum'],
-}
-
-
-df = pd.read_csv('./war-state-date-death.csv')
-grouped = df.groupby(['WarNum'])
-aggregated = grouped.agg(f)
-
-aggregated['Ccode'] = grouped['Ccode'].apply(list)
-aggregated['Player'] = grouped['Player'].apply(list)
-aggregated['WhereFought'] = grouped['WhereFought'].apply(set)
-
-aggregated.to_csv('./wars.csv')
+result_df = edge_df.loc[(edge_df.Ccode != edge_df.Ccode2) & (edge_df.Side != edge_df.Side2)]
+result_df.to_csv('./edges2.csv')
